7a123db6c63185c01a3ef2998ca55cbda2e92132
andrei-sk <dev@andrei.sk>
2025-10-25 12:14:46 +0200

    🐛 Fix JVM starting point

--- composeApp/src/jvmMain/kotlin/sk/andrei/jiratimetrackerpro/main.kt
+++ composeApp/src/jvmMain/kotlin/sk/andrei/jiratimetrackerpro/main.kt
@@ -1,16 +1,34 @@
 package sk.andrei.jiratimetrackerpro
 
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
-import sk.andrei.jiratimetrackerpro.presentation.feature.root.RootComponentImpl
+import org.koin.core.context.startKoin
+import sk.andrei.jiratimetrackerpro.data.dataModule
+import sk.andrei.jiratimetrackerpro.domain.domainModule
+import sk.andrei.jiratimetrackerpro.infrastructure.infrastructureModule
+import sk.andrei.jiratimetrackerpro.infrastructure.platformInfrastructureModule
+import sk.andrei.jiratimetrackerpro.presentation.feature.root.component.RootComponentImpl
+import sk.andrei.jiratimetrackerpro.presentation.feature.root.ui.RootScreen
 
+/**
+ * JVM's starting point
+ */
 fun main() {
+    startKoin {
+        modules(
+            dataModule,
+            infrastructureModule,
+            platformInfrastructureModule,
+            domainModule
+        )
+    }
+
     val lifecycle = LifecycleRegistry()
 
     val rootComponent = runOnUiThread {
@@ -28,9 +46,10 @@ fun main() {
             title = "My Application"
         ) {
             MaterialTheme {
-                Surface {
-                    App(rootComponent)
-                }
+                RootScreen(
+                    modifier = Modifier,
+                    component = rootComponent,
+                )
             }
         }
     }
